68200737c63489817cdbacffe98ce4e79ed29583
Htin Wana <Htinwana.dev@gmail.com>
2019-11-18 18:06:34 +0630

    added mailto button

---
+++ src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ContactMe extends Component {
+    state = {}
+    render() {
+        return (<div className='col s12 m6 l4 center-align'>
+            <h5 style={{ color: "#E5E9F0", paddingBottom: "10%" }}>
+                To get in touch with me or just wanna drop by and say hi
+        </h5>
+
+            <a href="mailto:htinwana.dev@gmail.com">
+                <i className="fa fa-envelope" style={{ fontSize: "5rem", color: "#E5E9F0" }} />
+            </a>
+            <h6 style={{ color: "#E5E9F0" }}>htinwana@gmail.com</h6>
+        </div>);
+    }
+}
+
+export default ContactMe;
