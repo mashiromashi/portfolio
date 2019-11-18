55a7779c975fe83b5386391676081275604f4600
Htin Wana <Htinwana.dev@gmail.com>
2019-11-18 17:23:26 +0630

    fixed the excess space in nav bar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,31 +6,30 @@ class NavBar extends Component {
     return (
       <nav className='nav-extended' style={{ backgroundColor: "#2E3440" }}>
         <div className='nav-content '>
-          <div className='row'>
-            <div className='col s12'>
-              <ul className='tabs tabs-transparent '>
-                <li className='tab'>
-                  <a href='#me' id='meTab' className='active'>
-                    Me
+
+          <div className='col s12'>
+            <ul className='tabs tabs-transparent '>
+              <li className='tab'>
+                <a href='#me' id='meTab' className='active'>
+                  Me
                   </a>
-                </li>
-                <li className='tab '>
-                  <a href='#skills' id='skillTab'>
-                    Skills
+              </li>
+              <li className='tab '>
+                <a href='#skills' id='skillTab'>
+                  Skills
                   </a>
-                </li>
-                <li className='tab '>
-                  <a href='#works' id='workTab'>
-                    My Works
+              </li>
+              <li className='tab '>
+                <a href='#works' id='workTab'>
+                  My Works
                   </a>
-                </li>
-                <li className='tab '>
-                  <a href='#contactMe' id='contactMeTab'>
-                    Contact Me
+              </li>
+              <li className='tab '>
+                <a href='#contactMe' id='contactMeTab'>
+                  Contact Me
                   </a>
-                </li>
-              </ul>
-            </div>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
