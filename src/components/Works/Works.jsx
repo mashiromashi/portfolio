d9f0755a5fde52ca6f03f6ac236fc1d4f2324d9a
Htin Wana <htinwana.dev@gmail.com>
2019-11-24 01:14:22 +0630

    update css

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import FloodmapFront from './FloodmapFront/FloodmapFront';
-import FloodmapBack from './FloodmapBack/FloodmapBack/FloodmapBack';
-import Portfolio from './Portfolio/Portfolio';
+import React, { Component } from "react";
+import FloodmapFront from "./FloodmapFront/FloodmapFront";
+import FloodmapBack from "./FloodmapBack/FloodmapBack/FloodmapBack";
+import Portfolio from "./Portfolio/Portfolio";
 
 class Works extends Component {
-    state = {}
-    render() {
-        return (
-            <div className="row">
-                <div className="center-align">
-                    <h3 style={{ color: "#D8DEE9" }}>Personal Projects</h3>
-                </div>
-                <FloodmapFront />
-                <FloodmapBack />
-                <Portfolio />
-            </div>
-        )
-    }
-
+  state = {};
+  render() {
+    return (
+      <div className='row'>
+        <div className='center-align'>
+          <h3 style={{ color: "#D8DEE9" }}>Personal Projects</h3>
+        </div>
+        <FloodmapFront />
+        <FloodmapBack />
+        <Portfolio />
+      </div>
+    );
+  }
 }
 export default Works;
